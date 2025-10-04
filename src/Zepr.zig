13c2c1c47bafeefda6d33aaa1b9f80876c7634ee
musaubrian <musaubrian15@gmail.com>
2025-10-04 11:32:05 +0300

    fix: bind the frag shader

--- src/Zepr.zig
+++ src/Zepr.zig
@@ -14,12 +14,14 @@ pub fn create(name: []const u8) Zepr {
 }
 
 pub fn addVertexShader(zepr: *Zepr, source: []const u8) !void {
+    if (source.len == 0) return error.EmptyVertexShaderSource;
     zepr.vertex_shader = c.glCreateShader(c.GL_VERTEX_SHADER);
     c.glShaderSource(zepr.vertex_shader.?, 1, &source.ptr, null);
     c.glCompileShader(zepr.vertex_shader.?);
 }
 
 pub fn addFragmentShader(zepr: *Zepr, source: []const u8) !void {
+    if (source.len == 0) return error.EmptyFragmentShaderSource;
     zepr.fragment_shader = c.glCreateShader(c.GL_FRAGMENT_SHADER);
     c.glShaderSource(zepr.fragment_shader.?, 1, &source.ptr, null);
     c.glCompileShader(zepr.fragment_shader.?);
@@ -29,8 +31,7 @@ pub fn compile(zepr: *Zepr) !void {
     errdefer c.glDeleteProgram(zepr.id);
     if (zepr.vertex_shader == null or zepr.fragment_shader == null) unreachable;
     c.glAttachShader(zepr.id, zepr.vertex_shader.?);
-
-    c.glAttachShader(zepr.id, zepr.vertex_shader.?);
+    c.glAttachShader(zepr.id, zepr.fragment_shader.?);
     c.glLinkProgram(zepr.id);
     defer {
         c.glDeleteShader(zepr.vertex_shader.?);
